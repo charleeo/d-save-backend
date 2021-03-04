d927177534a26ba01128ddf2e831d9a84bd58d4f
proskool <christiana@proxynetgroup.com>
2021-03-04 16:31:32 +0100

    configured the sequelise configuration

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const db_username = process.env.DB_USERNAME
 const db_password = process.env.DB_PASSWORD
-const db_database = process.env.DB_DATABSE
+const db_database = process.env.DB_DATABASE
 const db_host = process.env.DB_HOST
 const config ={
   development: {
