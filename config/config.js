8e5970c6d8a365312839d40706c457066058c0e1
proskool <christiana@proxynetgroup.com>
2021-03-04 16:14:29 +0100

    configured the sequelise configuration

---
+++ config/config.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+
+const db_username = process.env.DB_USERNAME
+const db_password = process.env.DB_PASSWORD
+const db_database = process.env.DB_DATABSE
+const db_host = process.env.DB_HOST
+const config ={
+  development: {
+    username: "root",
+    password: null,
+    database: "mobile_wallet",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    operatorsAliases: 0
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    operatorsAliases: 0
+  },
+  production: {
+    username: db_username,
+    password: db_password,
+    database: db_database,
+    host: db_host,
+    dialect: "mysql",
+    operatorsAliases: 0
+  }
+}
+module.exports=config
