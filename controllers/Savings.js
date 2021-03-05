9a553605cf8a0398157f5f84da361dd2204e6407
proskool <christiana@proxynetgroup.com>
2021-03-05 11:38:25 +0100

    New changes

--- controllers/Savings.js
+++ controllers/Savings.js
@@ -1,13 +1,29 @@
 const Models = require("../models")
 const winston = require('winston')
 const fsPromises = require('fs').promises
+const crypto = require('crypto');
+const { string } = require("joi");
 
 async function receivePayment(req,res){
   const data = req.body;
-  winston.info({data})
-  winston.info({AccountPayments:data.accountPayments})
-  // `${clientSecret}|${paymentReference}|${amountPaid}|${paidOn}|${transactionReference}`
+  // winston.info({data})
+  winston.info({AccountPayments:data.transactionHash})
+
+  const paymentReference = data.paymentReference
+  const transactionReference= data.transactionReference
+  const amountPaid = data.amountPaid
+  const paidOn = data.paidOn
+  const clientSecret = process.env.MONNIFY_SECRET_KEY
+  const stringValue=`${clientSecret}|${paymentReference}|${amountPaid}|${paidOn}|${transactionReference}`
   // await fsPromises.writeFile('file.txt', `${data}`)
+    key = process.env.MONNIFY_SECRET_KEY
+
+// create hahs
+const hash = crypto.createHmac('sha512', key)
+hash.update(stringValue)
+const value = hash.digest('hex')
+winston.info(value)
+
 }
 // 3000023360 
 
