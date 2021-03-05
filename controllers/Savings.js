8050ca2de8d147db1167af695629fa7d997c64fa
proskool <christiana@proxynetgroup.com>
2021-03-05 09:18:49 +0100

    Fix a bug

--- controllers/Savings.js
+++ controllers/Savings.js
@@ -6,7 +6,7 @@ async function receivePayment(req,res){
  const responseBody= req.body
  fs.writeFile('response.json', responseBody, function (err) {
   if (err) throw err;
-  winston.info('It\'s saved!');
+  winston.info(responseBody);
 });
 }
 
