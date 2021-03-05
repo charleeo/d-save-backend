5e062c3a7a2b9a8cc62bc04c20e34ed302a95c37
proskool <christiana@proxynetgroup.com>
2021-03-05 06:19:19 +0100

    Migration modified

--- migrations/20210225035650-modify_account_reservattion_info.js
+++ migrations/20210225035650-modify_account_reservattion_info.js
@@ -40,8 +40,8 @@ module.exports = {
       queryInterface.removeColumn('reserved_accounts', 'accountName'),
       queryInterface.removeColumn('reserved_accounts', 'status'),
       queryInterface.removeColumn('reserved_accounts', 'accountNumber'),
-      queryInterface.removeColumn('reserved_accounts', 'accountNumber'),
-      queryInterface.removeColumn('reserved_accounts', 'collectionChannel'),
+      queryInterface.removeColumn('reserved_accounts', 'bankCode'),
+      queryInterface.removeColumn('reserved_accounts', 'collectionChanell'),
       queryInterface.removeColumn('reserved_accounts', 'reservationReference'),
       
     ]);
