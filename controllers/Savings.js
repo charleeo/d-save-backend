1b23c584c4445a4d6946fb221fe93c7b8094d098
proskool <christiana@proxynetgroup.com>
2021-03-05 09:48:23 +0100

    Fix a bug

--- controllers/Savings.js
+++ controllers/Savings.js
@@ -1,12 +1,12 @@
 const Models = require("../models")
 const winston = require('winston')
 
-const { writeFile } = require('fs');
+const fs  = require('fs');
 
 async function receivePayment(req,res){
 
 const data = new Uint8Array(Buffer.from(req.body));
-writeFile('savings.txt', data, (err) => {
+fs.writeFile('savings.txt', data, (err) => {
   if (err) throw err;
   winston.info('The file has been saved!');
 });
