148532a045f06d4b551e77e7f131171d9ecddbf3
proskool <christiana@proxynetgroup.com>
2021-03-05 05:46:48 +0100

    Made somechages

--- models/index.js
+++ models/index.js
@@ -4,15 +4,13 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV;
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-console.log(config)
 
 let sequelize;
 if (config.use_env_variable) {
-  console.log('Here in the first condition')
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
