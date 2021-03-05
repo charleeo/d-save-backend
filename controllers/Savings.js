52d86a02eaa9a12e95179752fd8d4351d05e6401
proskool <christiana@proxynetgroup.com>
2021-03-05 10:57:44 +0100

    New changes

--- controllers/Savings.js
+++ controllers/Savings.js
@@ -4,8 +4,10 @@ const fsPromises = require('fs').promises
 
 async function receivePayment(req,res){
   const data = req.body;
-  winston.info(data)
+  winston.info({AccountDetails:data.accountDetails})
+  winston.info({AccountPayments:data.accountPayments})
   await fsPromises.writeFile('file.txt', `${data}`)
 }
+3000023360 
 
 module.exports= {receivePayment}
