892991c4f952ac7a66009a0589b8b638c9c02c5f
proskool <christiana@proxynetgroup.com>
2021-04-24 12:38:19 +0100

    made some changes

--- controllers/getVar.js
+++ controllers/getVar.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const forAPI=(req,res)=>{
   const apiVars = {key:process.env.REACT_APP_MONNIFY_SECRET_KEY,contract:process.env.REACT_APP_MONNIFY_CONTRACT_CODE}
-  console.log(apiVars)
  return  res.json({res:apiVars})
 }
 
