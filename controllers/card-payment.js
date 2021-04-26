0f3cf52b1a33953481e105f3b5869612fc3e0614
proskool <christiana@proxynetgroup.com>
2021-04-26 17:49:37 +0100

    new changes

--- controllers/card-payment.js
+++ controllers/card-payment.js
@@ -1,13 +1,13 @@
 const models = require("../models")
 const winston = require('winston')
-const savingsObject = require('./depsoits')
-const {depositHistory} = require('./depositHostory')
+const savingsObjectOnline = require('./depsoits_online')
+const {depositHistoryOnline} = require('./depositHostory_online')
 require('dotenv').config()
 
 async function receiveCardPayment(req,res){
   const postData = req.body;
 if(postData.transactionStatus==='SUCCESS'){
- const savingHistory = new models.DepositHistory(depositHistory(postData))
+ const savingHistory = new models.DepositHistory(depositHistoryOnline(postData))
  await savingHistory.save();
  await savingsObject(postData);
  return res.status(201).json({message:"Your deposit was recieved by us"}) ;
---
+++ controllers/depsoits_online.js
@@ -0,0 +1,57 @@
+const winston = require('winston');
+const models = require('../models/index');
+
+const formatTimeStamp=(timeStamp)=>{
+  return new Date(timeStamp)
+}
+const savingsObjectOnline= async(params)=>{
+  const plans={bronze:5000,silver:25000};
+   const {transactionHash,customer, transactionReference,paidOn,amountPaid,settlementAmount} =params;
+   console.log(customer)
+   let date = new Date();
+   let minLiquidationMonths = date.setUTCMonth(date.getUTCMonth()+6);
+   let minLiquidationWeeks =  new Date().getTime()+(7*24*60*60*1000);//7 days added to current time/date
+   let investmentDuration;
+   let interestRate;
+   let expectedInterest;
+   let actualInterest;
+   let liquidationPeriod;
+   let liquidatedDate;
+   let investmentCategory;
+
+   let data={investmentDuration,interestRate,expectedInterest,actualInterest,    liquidatedDate, liquidationPeriod,cutomerEmail:customer.email,cutomerName:customer.name,investmentAmount:amountPaid, investmentCategory,transactionReference,paidOn,transactionHash,settlementAmount}
+   data.liquidatedDate = formatTimeStamp(minLiquidationMonths)
+
+   
+
+   if(amountPaid <= plans.bronze){
+    //  Do somethind for the savings table
+    data.investmentDuration= formatTimeStamp(minLiquidationWeeks)
+    data.interestRate=0;
+    data.investmentCategory=1;
+    data.liquidatedDate=formatTimeStamp(minLiquidationWeeks)
+    data.actualInterest = 0;
+    const savingsItem= new models.Saving(data)
+   return await savingsItem.save()
+  }else{
+  
+   if(amountPaid > plans.bronze && amountPaid <= plans.silver){
+      //  Investment category will be silver
+      data.investmentCategory = 2;
+      data.interestRate=10;
+      data.actualInterest = parseInt((amountPaid) *(data.interestRate/100))
+    }else if(amountPaid > plans.silver){
+      //  Investment category will be gold
+      data.investmentCategory = 3;
+      data.interestRate=15;
+      data.actualInterest = parseInt((amountPaid) *(data.interestRate/100))
+    }
+    data.investmentDuration = data.liquidationPeriod= formatTimeStamp(minLiquidationMonths);
+    
+    const investItems = new models.InvestmentsDetails(data);
+   return await investItems.save()
+  }
+  
+}
+
+module.exports = savingsObject
