a22976e9f3408ae56d8bad017de4130666ba43a8
charleeo <charleeotaru@gmail.com>
2021-05-01 21:57:17 +0100

    started the disbursement api integration

--- middleware/authenticate_gateway.js
+++ middleware/authenticate_gateway.js
@@ -1,21 +1,21 @@
 const axios = require('axios');
 require('dotenv').config()
-const auth =require('../middleware/monnify_configs')
+axios.defaults.baseURL = "https://sandbox.monnify.com/api/";
+const username = process.env.MONNIFY_SECRET_KEY;
+const password = process.env.MONNIFY_PASSWORD;
+
 async function authenticateGateWay(){
+
     try {
-      
-      const response = await axios({
-        url: 'v1/auth/login/',
-        method: 'post',
-        headers: {
-            Authorization: auth(),
-        },
-            
-    });
+  const response=  await  axios.post(
+        `v1/auth/login/`,
+        {},
+        {auth:{username,password}}
+      )
     return (response.data.responseBody.accessToken)
     } catch (error) {
-      console.log(error)
+     console.log(error.message)
     }
 }
 
-module.exports={authenticateGateWay}
+module.exports=authenticateGateWay
