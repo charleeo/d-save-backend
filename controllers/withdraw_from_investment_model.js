88c43113f412a0a15adfa42d45e7f3be25bcbd96
charleeo <charleeotaru@gmail.com>
2021-05-08 11:50:18 +0100

    new updates

--- controllers/withdraw_from_investment_model.js
+++ controllers/withdraw_from_investment_model.js
@@ -20,13 +20,15 @@ const withDrawInvestment=async(data)=>{
 }
 module.exports=withDrawInvestment
 
-
-//ensure the account details suppplied are valid
-// const validateAccountNumber = await axios({
-//   url: `v1/disbursements/account/validate?accountNumber=${destinationAccountNumber}&bankCode=${destinationBankCode}`,
-//   method:'get',
-// });
-// if(validateAccountNumber.requestSuccessful !==true && validateAccountNumber.responseMessage !=='success'){
-//   return res.status(400).json({error:`The account number ${destinationAccountNumber} does not match with the bankcode ${destinationBankCode}. Please try again`})
-// }
+function name(params) {
+  
+  //ensure the account details suppplied are valid
+  // const validateAccountNumber = await axios({
+  //   url: `v1/disbursements/account/validate?accountNumber=${destinationAccountNumber}&bankCode=${destinationBankCode}`,
+  //   method:'get',
+  // });
+  // if(validateAccountNumber.requestSuccessful !==true && validateAccountNumber.responseMessage !=='success'){
+  //   return res.status(400).json({error:`The account number ${destinationAccountNumber} does not match with the bankcode ${destinationBankCode}. Please try again`})
+  // }
+}
 
