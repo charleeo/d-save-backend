6149e610fe99c7dd031ee26ddd7b9811d48c5a22
charleeo <charleeotaru@gmail.com>
2021-05-12 08:52:49 +0100

    Withdrawals completion

--- controllers/withdrawalsHistory.js
+++ controllers/withdrawalsHistory.js
@@ -1,9 +1,11 @@
 const models = require('../models/index');
 const Sequelize = require('sequelize')
 
-export const withdrawalHistoryPerPrseon=(req,res)=>{
+ const withdrawalHistoryPerPrseon=(req,res)=>{
   const email = req.params.email;
   const history = models.WithdrawalHistory.findAll({where:{userEmail:email}});
   if(!history)return res.status(404).json({error:"No record found"});
   res.status(200).json({message:history})
 }
+
+module.exports={withdrawalHistoryPerPrseon}
