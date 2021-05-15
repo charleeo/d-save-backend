84830e8d78de1dd0651a720a3612509eea167f63
charleeo <charleeotaru@gmail.com>
2021-05-15 15:58:18 +0100

    fixed a multiple withdrawals bug

--- controllers/withdrawl_from_savings.js
+++ controllers/withdrawl_from_savings.js
@@ -5,7 +5,7 @@ async function getSavingsWithIDs(data){
   let statusCode = 0;
   
   let {investmentID}= data
-  console.log(investmentID)
+  console.log(investmentID.split([]))
    investmentID = investmentID.replace(/['"]+/g, '');
    console.log(investmentID)
   const savingsWithIDs= await models.Saving.findAll(
@@ -17,7 +17,7 @@ async function getSavingsWithIDs(data){
    error='resource not found';
    statusCode=404
   }
-  console.log(savingsWithIDs)
+  console.log(savingsWithIDs.length)
   return {
     statusCode,
     savingsWithIDs,
