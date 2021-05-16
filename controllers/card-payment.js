a2bf40fa16d0e8936eae8b88c78e1050b555bad3
charleeo <charleeotaru@gmail.com>
2021-05-17 00:28:41 +0100

    re-iinvestment functionality

--- controllers/card-payment.js
+++ controllers/card-payment.js
@@ -12,12 +12,15 @@ async function receiveCardPayment(req,res){
   if(postData.transactionStatus==='SUCCESS'){
     if(postData.type !==undefined && postData.type =='re-investment'){
       //update the savings table and set the rows whose ids are in the request to false
+      if(postData.amountPaid <=5000){
+        return res.status(400).json({error:"The minimum amount for investment must be greater than 5000. I.e 5100 and above"})
+      }
       let ids =  postData.investmentID
       const {Code,savingsError} = await getSavingsWithIDs(postData);
       if(savingsError !==''){
         return res.status(Code).json({error:savingsError})
       }
-      
+
       ids = ids.split(',')
       await models.Saving.update(
         {status:false},
