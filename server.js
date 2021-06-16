e608f865f401e9e9c7d245c1f75c5b2d5af98c9d
charleeo <charleeotaru@gmail.com>
2021-06-16 22:12:13 +0100

    New Stuff

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const winston = require('winston')
 const http = require('http');
 const app = require('./app');
 require('dotenv').config();
-const port = process.env.PORT;
+const port = process.env.PORT ||5000;
 const server = http.createServer(app);
 server.listen(port, ()=>{
   winston.info(`Listening on port ${port}`)
